df820b0b758f0d5b2ac9e6c55ba573ad27bf4f57
Vince Quiles <vjquiles@msn.com>
2025-07-17 18:54:51 -0400

    Tax calculator added, i also logged the tax for product 1 just to make sure the right tax is being applied. I just have to switch the category to gorceries to be 100 percent, i will try that later.

---
+++ src/util/taxCalculator.ts
@@ -0,0 +1,9 @@
+import Product from '../models/Product'
+
+export function calculateDiscount(product: Product): number {
+    return product.price * product.tax
+}
+
+const product1 = new Product(1, 'title', 'category', 2, 3, 4, 5, 6)
+console.log(calculateDiscount(product1))
+console.log(product1.tax)

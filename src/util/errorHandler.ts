03543eeeb9a72832d1da29b021ecadcdd00bc3bc
Vince Quiles <vjquiles@msn.com>
2025-07-17 20:58:24 -0400

    Error handler being added, has not been tested yet. Logging progress for now.

---
+++ src/util/errorHandler.ts
@@ -0,0 +1,26 @@
+export class NetworkError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = 'Network Error'
+    }
+}
+
+export class DataError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = 'Data Error'
+    }
+}
+
+export function validateNetwork(response: Response) {
+    if (!response.ok) {
+        throw new NetworkError(`Error: ${response.status}`)
+    }
+}
+
+export function ValidateData(data: any) {
+    if (typeof data !== 'object') {
+        throw new DataError('Error validating data, please try again')
+    }
+}
+

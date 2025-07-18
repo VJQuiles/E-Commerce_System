078ef94f7f1cd3724baabea6c90cc6ea534d088b
Vince Quiles <vjquiles@msn.com>
2025-07-18 19:50:37 -0400

    Got it working properly, I needed to return the products from productData after the valididty check, and then when logging, i needed to take a callback function once the promise resolved, which is why i was getting pending just trying to call the method in the log

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,18 +1,21 @@
+import Product from "../models/Product"
 import { NetworkError, DataError } from "../util/errorHandler"
 
-export async function fetchProductData() {
+export async function fetchProductData(): Promise<Product[]> {
     try {
         const response = await fetch('https://dummyjson.com/products?limit=10&select=id,title,category,price,discountPercentage')
         if (!response.ok) {
             throw new NetworkError("Error connecting to network.")
         }
         const productData = await response.json()
-        if (productData !== "object" || productData === null) {
+        if (typeof productData !== "object" || productData === null) {
             throw new DataError("Error parsing data. It's in French. I took latin in high school. I wanted to talk like Yoda.")
         }
+        return productData.products
     }
     catch (error) {
-
+        throw error
     }
 }
-console.log(fetchProductData())
+
+//fetchProductData().then(data => console.log(data))

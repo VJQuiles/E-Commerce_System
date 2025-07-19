34c74f394197de7a27428ea97c1a3069141b80ba
Vince Quiles <vjquiles@msn.com>
2025-07-19 11:48:59 -0400

    Wired up main with only displayDetails to check wiring. I did get a result, just not the one i was expecting. I got undefined but i expected my displayDetails string. Time to go hunting

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,25 @@ import { fetchProductData } from "./services/apiService"
 import { NetworkError, DataError } from "./util/errorHandler"
 import { calculateDiscount } from "./util/discountCalculator"
 import { calculateTax } from "./util/taxCalculator"
+
+
+async function doTheThings() {
+    const productData = await fetchProductData()
+    const productList = productData.map(
+        (products) =>
+            new Product(
+                products.id,
+                products.title,
+                products.category,
+                products.price,
+                products.discountPercentage,
+                products.tax)
+    )
+
+    productList.forEach(
+        (product) =>
+            product.displayDetails()
+    )
+}
+
+doTheThings().then((result) => console.log(result))
